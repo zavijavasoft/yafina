614c9e9a87120acc015daf9b542640c55f863348
Олег Будин <okbudin@yandex.ru>
2018-07-20 12:29:56 +0300

    Начальный коммит проекта =из коробки=

---
+++ app/src/main/java/com/zavijavasoft/yafina/BalanceActivity.kt
@@ -0,0 +1,12 @@
+package com.zavijavasoft.yafina
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BalanceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_balance)
+    }
+}
