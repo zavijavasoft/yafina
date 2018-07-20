fabaadb79ae59c89909d76d0567a62ae80c7408b
Олег Будин <okbudin@yandex.ru>
2018-07-20 21:50:29 +0300

    Добавляем объекты-заглушки для источников данных
    На данном этапе мы работаем с "хардкодными" данными, которые и предоставляются этими заглушками

---
+++ app/src/main/java/com/zavijavasoft/yafina/stub/StubCurrencyMonitor.kt
@@ -0,0 +1,46 @@
+package com.zavijavasoft.yafina.stub
+
+import com.zavijavasoft.yafina.core.AbstractCurrencyMonitor
+import com.zavijavasoft.yafina.core.CurrencyExchangeRatio
+import com.zavijavasoft.yafina.core.CurrencyStorage
+import java.util.*
+
+
+class StubCurrencyStorage : CurrencyStorage {
+    override fun getCurrencyList(): List<String> {
+        return listOf("USD", "RUR")
+    }
+
+    override fun addCurrency(currency: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun removeCurrency(currency: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
+
+class StubCurrencyMonitor : AbstractCurrencyMonitor() {
+
+
+    override val ratios: List<CurrencyExchangeRatio>
+
+
+    init {
+        val usdrur = CurrencyExchangeRatio("USD", "RUR", 63.46f, Date())
+        val rurusd = CurrencyExchangeRatio("RUR", "USD", 1.0f / 63.46f, Date())
+        ratios = listOf(usdrur, rurusd)
+    }
+
+    override fun update() {
+        for (lis in listeners) {
+            lis(ratios)
+        }
+    }
+
+
+    override fun pull(): List<CurrencyExchangeRatio> {
+        return ratios
+    }
+}
