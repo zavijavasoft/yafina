b6532c66dbeb5f8ea9775a228a6faf6937a59800
Олег Будин <okbudin@yandex.ru>
2018-07-23 05:21:37 +0300

    Добавлены элементы представления для  вкладки About

---
+++ app/src/main/java/com/zavijavasoft/yafina/ui/AboutFragment.kt
@@ -0,0 +1,33 @@
+package com.zavijavasoft.yafina.ui
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.zavijavasoft.yafina.R
+
+
+class AboutFragment : Fragment() {
+
+    companion object {
+        val TAG_YAFINA_ABOUT_FRAGMENT = "TAG_YAFINA_ABOUT_FRAGMENT"
+
+        fun getInstance(): AboutFragment {
+            val fragment = AboutFragment()
+            val args = Bundle()
+            fragment.arguments = args
+            return fragment
+        }
+
+    }
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+
+}
