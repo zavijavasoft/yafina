f80e9542be676233f6d2c1ebd8f5e0f84b744670
Олег Будин <okbudin@yandex.ru>
2018-07-30 07:31:23 +0300

    Фиксация глюка гита в AS

---
+++ app/src/main/java/com/zavijavasoft/yafina/OpenTransactionActivity.kt
@@ -0,0 +1,12 @@
+package com.zavijavasoft.yafina
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class OpenTransactionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_transaction)
+    }
+}
