1b3f031d3265f3371ea77ed46ed5a19068474a2d
Олег Будин <okbudin@yandex.ru>
2018-07-31 20:32:59 +0300

    Поправлен критический баг
    Баг: Moxy пыталась повторить requireTransaction после смены
    конфигурации, воскрешая убитую активити,но без корректного интента, что
    при повторной смене конфигурации приводило к крашу
    В дальнейшем я хотел превратить эту активити во фрагмент, где эта
    проблема была бы неактуальной
    Решено сменой стратегии для данного метода

--- app/src/main/java/com/zavijavasoft/yafina/ui/operation/OperationView.kt
+++ app/src/main/java/com/zavijavasoft/yafina/ui/operation/OperationView.kt
@@ -2,6 +2,8 @@ package com.zavijavasoft.yafina.ui.operation
 
 import android.os.Parcelable
 import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.SkipStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 import com.zavijavasoft.yafina.model.AccountEntity
 import com.zavijavasoft.yafina.model.ArticleEntity
 import kotlinx.android.parcel.Parcelize
@@ -26,5 +28,6 @@ data class TransactionRequest(val type: TransactionType,
 interface OperationView : MvpView {
     fun update(rests: Map<Long, Float>, arcticles: List<ArticleEntity>, accounts: List<AccountEntity>)
     fun notifyInsufficientMoney(accountTitle: String, sumExists: String, sumRequired: String)
+    @StateStrategyType(SkipStrategy::class)
     fun requireTransaction(request: TransactionRequest)
 }
